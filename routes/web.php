a4a472f5ea043b8d0c6d04e48be659260a38d9dc
Gilly Willy <58342490+Willy-Gilly@users.noreply.github.com>
2021-10-05 17:24:02 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 // -------- Error Pages ------------------ useless but they're there to test
-Route::get('/error404','View\HomeViewController@getNotFoundPage')->name('error404');
-Route::get('/error403','View\HomeViewController@getRightErrorPage')->name('error403');
-Route::get('/error401','View\HomeViewController@getUnauthorizedPage')->name('error401');
-Route::get('/error419','View\HomeViewController@getAuthenticationTimoutPage')->name('error419');
-Route::get('/error429','View\HomeViewController@getTooManyRequestPage')->name('error429');
-Route::get('/error500','View\HomeViewController@getInternalErrorPage')->name('error500');
-Route::get('/error503','View\HomeViewController@getServiceUnavailablePage')->name('error503');
+Route::get('/error404','App\Http\Controllers\View\ViewController@getNotFoundPage')->name('error404');
+Route::get('/error403','App\Http\Controllers\View\ViewController@getRightErrorPage')->name('error403');
+Route::get('/error401','App\Http\Controllers\View\ViewController@getUnauthorizedPage')->name('error401');
+Route::get('/error419','App\Http\Controllers\View\ViewController@getAuthenticationTimoutPage')->name('error419');
+Route::get('/error429','App\Http\Controllers\View\ViewController@getTooManyRequestPage')->name('error429');
+Route::get('/error500','App\Http\Controllers\View\ViewController@getInternalErrorPage')->name('error500');
+Route::get('/error503','App\Http\Controllers\View\ViewController@getServiceUnavailablePage')->name('error503');
 // --------- End Error Pages ------------------
 
 //Change Language
